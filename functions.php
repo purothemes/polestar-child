f37095c20d8d81cee959292f6d84e57f954ca156
Andrew Misplon <amisplon@gmail.com>
2017-05-26 11:50:55 +0200

    Small change to enqueue function comments

--- functions.php
+++ functions.php
@@ -1,9 +1,12 @@
 <?php
 
 /**
- * Enqueue the parent theme stylesheet.
+ * Enqueue theme scripts and styles.
  */
 function polestar_child_scripts() {
+
+	// Parent theme stylesheet.
     wp_enqueue_style( 'polestar-parent-style', get_template_directory_uri() . '/style.css' );
+
 }
 add_action( 'wp_enqueue_scripts', 'polestar_child_scripts', 8 );
