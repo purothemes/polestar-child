85e835ce3ffea35faee33f22347e4183737b05e7
Andrew Misplon <amisplon@gmail.com>
2018-01-05 11:12:47 -0800

    Fix duplicate parent stylesheet issue

--- functions.php
+++ functions.php
@@ -5,8 +5,8 @@
  */
 function polestar_child_scripts() {
 
-	// Parent theme stylesheet.
-    wp_enqueue_style( 'polestar-parent-style', get_template_directory_uri() . '/style.css' );
+	// Child theme stylesheet.
+	wp_enqueue_style( 'polestar-child', get_stylesheet_directory_uri() . '/style.css' );
 
 }
 add_action( 'wp_enqueue_scripts', 'polestar_child_scripts', 8 );
